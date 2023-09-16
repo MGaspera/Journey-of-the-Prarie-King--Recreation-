1b0e26ca306728c47820f8e873bc322b169b89cc
SosoG-null <144150345+MGaspera@users.noreply.github.com>
2023-09-16 18:17:06 +0100

    shooting and general improvements

--- Classes.py
+++ Classes.py
@@ -1,20 +1,60 @@
 import pygame
 import os
+import math
 
-class PlayerInfo():
+
+
+class player():
     def __init__(self):
         self.x = 0
         self.y = 0
-        self.width = 50
+        self.width = 30
         self.height = 50
-        self.vel = 5
+        self.vel = 0.1
 
-class Anim():
-    def __init__(self):
-        self.walk= [pygame.image.load(os.path.join("./Assets/Necro/Move/", f'Move ({i}).png')) for i in range(1, 22)]
-        self.death= [pygame.image.load(os.path.join("./Assets/Necro/Death/", f'Death ({i}).png')) for i in range(1, 10)]
-        self.idle= [pygame.image.load(os.path.join("./Assets/Necro/Idle/", f'Idle ({i}).png')) for i in range(1, 9)]
-        self.ult= [pygame.image.load(os.path.join("./Assets/Necro/Ult/", f'Ult ({i}).png')) for i in range(1, 14)]
-        self.bg = pygame.image.load(os.path.join("./Assets/Bg", f'Ground.jpg'))
+        self.walk= [pygame.image.load(os.path.join("./Assets/Necro/Move/", f'Move-({i}).png')) for i in range(1, 22)]
+        self.death= [pygame.image.load(os.path.join("./Assets/Necro/Death/", f'Death-({i}).png')) for i in range(1, 10)]
+        self.idle= [pygame.image.load(os.path.join("./Assets/Necro/Idle/", f'Idle-({i}).png')) for i in range(1, 9)]
+        self.ult= [pygame.image.load(os.path.join("./Assets/Necro/Ult/", f'Ult-({i}).png')) for i in range(1, 14)]
 
         self.leftIdle = [pygame.transform.flip(image, True, False) for image in self.idle]
+
+    def collide(self, spriteGroup):
+        if pygame.sprite.spritecollide(self, spriteGroup, False):
+            pass
+
+class screen():
+    def __init__(self):
+        self.bg = pygame.image.load(os.path.join("./Assets/Bg", f'Ground.jpg'))
+
+class bullet:
+    LEFT = (-1, 0)
+    RIGHT = (1, 0)
+    UP = (0, -1)
+    DOWN = (0, 1)
+
+    def __init__(self, x, y, direction):
+        self.x = x
+        self.y = y
+        self.speed = 0.1
+        self.direction = direction
+        self.rotation_angle = 0  # Initial rotation angle
+        self.original_image = pygame.Surface((20, 20), pygame.SRCALPHA)  # Create a transparent surface for the bullet
+        pygame.draw.polygon(self.original_image, (99, 10, 4), [(10, 0), (0, 20), (20, 20)])  # Create a triangular bullet
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+
+    def update(self):
+        # Move the bullet in the specified direction
+        self.x += self.speed * self.direction[0]
+        self.y += self.speed * self.direction[1]
+        self.rect.center = (self.x, self.y)
+
+        # Rotate the bullet (you can adjust the rotation speed by changing the angle increment)
+        self.rotation_angle += 0.1  # Adjust the rotation speed here
+
+        # Rotate the bullet image
+        self.image = pygame.transform.rotate(self.original_image, self.rotation_angle)
+
+    def draw(self, win):
+        win.blit(self.image, self.rect)
--- Main.py
+++ Main.py
@@ -1,21 +1,44 @@
 import pygame
-from Classes import PlayerInfo, Anim
+import time
+from Classes import screen, player, bullet as Bullet
 pygame.init()
 
-# Set display dimensions and create the window
-win_width, win_height = 500, 500
-win = pygame.display.set_mode((win_width, win_height))
-pygame.display.set_caption("Journey of the Prarie King")
+win = pygame.display.set_mode((500, 500), 0, 0)
+
+pygame.display.set_caption("Journey of the Prairie King")
 
 # Class Instances
-playerMaths = PlayerInfo()
-gameAnim = Anim()
+sprite = player()
+gameScreen = screen()
 
 # Initial sprite and animation variables
-spriteMain = gameAnim.idle[0]  # Set the initial sprite
+spriteMain = sprite.idle[0]  # Set the initial sprite
 animation_speed = 100  # Speed of animation (milliseconds)
 last_update = pygame.time.get_ticks()  # Get the initial time
 walkCount = 0  # Initialize walkCount
+
+def redrawGameWindow():
+    global walkCount, bullets
+    win.blit(gameScreen.bg, (0, 0))  # Blit the background first
+
+    # Draw left border rectangle
+    pygame.draw.rect(win, (84, 80, 69), (0, 0, 10, 500))
+    # Draw top border rectangle
+    pygame.draw.rect(win, (84, 80, 69), (0, 0, 500, 10))
+    # Draw bottom border rectangle
+    pygame.draw.rect(win, (84, 80, 69), (0, 490, 500, 10))
+    # Draw right border rectangle
+    pygame.draw.rect(win, (84, 80, 69), (490, 0, 10, 500))
+
+    # Update and draw bullets
+    for bullet in bullets:
+        bullet.update()  # Update bullet position
+        bullet.draw(win)  # Draw bullet on the screen
+
+    playerHitbox = pygame.Rect(sprite.x + 20, sprite.y + 35, sprite.width - 10, sprite.height - 10)
+    pygame.draw.rect(win, (255, 0, 0), playerHitbox, 2)  # Red outline
+    win.blit(spriteMain, (sprite.x, sprite.y))  # Blit the player sprite
+    pygame.display.update()  # Update the display
 
 right = False
 left = False 
@@ -25,14 +48,12 @@ screen_width = 500
 screen_height = 500
 
 # Define the border width (how close to the edge the player can get)
-border_width = 5  # You can adjust this value as needed
+border_width = 5  
 
-def redrawGameWindow():
-    global walkCount
-    win.blit(gameAnim.bg, (0, 0))  # Blit the background first
-    win.blit(spriteMain, (playerMaths.x, playerMaths.y))  # Blit the player sprite
-    pygame.display.update()  # Update the display
+# Create a list to store bullets
+bullets = []
 
+lastShotTime = 0
 run = True
 while run:
     for event in pygame.event.get():
@@ -40,17 +61,17 @@ while run:
             run = False
 
     # Get the current time
-    current_time = pygame.time.get_ticks()
+    currentTime = pygame.time.get_ticks()
 
     # Check if it's time to update the animation frame
-    if current_time - last_update >= animation_speed:
-        last_update = current_time
+    if currentTime - last_update >= animation_speed:
+        last_update = currentTime
 
         # Update the animation frame based on the direction (left or right)
         if right:
-            spriteMain = gameAnim.idle[walkCount % len(gameAnim.idle)]
+            spriteMain = sprite.idle[walkCount % len(sprite.idle)]
         elif left:
-            spriteMain = gameAnim.leftIdle[walkCount % len(gameAnim.leftIdle)]
+            spriteMain = sprite.leftIdle[walkCount % len(sprite.leftIdle)]
 
         # Increment the walkCount for the next frame
         walkCount += 1
@@ -58,51 +79,73 @@ while run:
     keys = pygame.key.get_pressed()
 
     # Player movement logic with adjusted boundaries
-    if keys[pygame.K_LEFT]:
-        playerMaths.x -= playerMaths.vel
+    if keys[pygame.K_a]:
+        sprite.x -= sprite.vel
         left = True
         right = False
 
-    if keys[pygame.K_RIGHT]:
-        playerMaths.x += playerMaths.vel
+    if keys[pygame.K_d]:
+        sprite.x += sprite.vel
         left = False
         right = True
 
+    if keys[pygame.K_w]:
+        sprite.y -= sprite.vel
+
+    if keys[pygame.K_s]:
+        sprite.y += sprite.vel
+
+    bullet_direction = (0, 0)
+
+    if keys[pygame.K_LEFT]:
+        bullet_direction = (-1, 0)  # Move left
+    elif keys[pygame.K_RIGHT]:
+        bullet_direction = (1, 0)  # Move right
+
     if keys[pygame.K_UP]:
-        playerMaths.y -= playerMaths.vel
+        bullet_direction = (0, -1)  # Move up
+    elif keys[pygame.K_DOWN]:
+        bullet_direction = (0, 1)  # Move down
 
-    if keys[pygame.K_DOWN]:
-        playerMaths.y += playerMaths.vel
+    # Combine horizontal and vertical directions for diagonal movement
+    if keys[pygame.K_LEFT] and keys[pygame.K_UP]:
+        bullet_direction = (-1, -1)  # Move up-left
+    elif keys[pygame.K_LEFT] and keys[pygame.K_DOWN]:
+        bullet_direction = (-1, 1)  # Move down-left
+    elif keys[pygame.K_RIGHT] and keys[pygame.K_UP]:
+        bullet_direction = (1, -1)  # Move up-right
+    elif keys[pygame.K_RIGHT] and keys[pygame.K_DOWN]:
+        bullet_direction = (1, 1)  # Move down-right
 
-    # Boundary checks to prevent the player from going out of the screen
-    if playerMaths.x < border_width:
-        playerMaths.x = border_width
+    # Calculate the time elapsed since the last shot
+    time_since_last_shot = currentTime - lastShotTime
 
-    if playerMaths.x > win_width - playerMaths.width - border_width:
-        playerMaths.x = win_width - playerMaths.width - border_width
+    # If enough time has passed (250 milliseconds) and there's a direction, allow the player to shoot
+    if time_since_last_shot >= 250 and bullet_direction != (0, 0):
+        # Create a new bullet with the current direction
+        new_bullet = Bullet(sprite.x + sprite.width // 2 + 5, sprite.y + sprite.height // 2 + 5, bullet_direction)
+        bullets.append(new_bullet)
 
-    if playerMaths.y < border_width:
-        playerMaths.y = border_width
+        # Update the time of the last shot
+        lastShotTime = currentTime
 
-    if playerMaths.y > win_height - playerMaths.height - border_width:
-        playerMaths.y = win_height - playerMaths.height - border_width
+    # Update bullets
+    for bullet in bullets:
+        bullet.update()
+
+    # Remove bullets that are out of bounds
+    bullets = [bullet for bullet in bullets if bullet.y > 0]  # Adjust the condition as needed
 
     # Clear the screen
     win.fill((0, 0, 0))
 
     # Blit the background
-    win.blit(gameAnim.bg, (0, 0))
-
-    # Draw rectangles
-    for i in range(0, win_width, 10):
-        pygame.draw.rect(win, (255, 0, 0), (i, i, 10, 10), 1)
+    win.blit(gameScreen.bg, (0, 0))
 
     # Blit the player sprite
-    win.blit(spriteMain, (playerMaths.x, playerMaths.y))
-
-    print("coords:", playerMaths.x, playerMaths.y)
+    win.blit(spriteMain, (sprite.x, sprite.y))
 
     # Update the display
-    pygame.display.update()
+    redrawGameWindow()
 
 pygame.quit()
