aaf67b2ac35992e2ebe9ba3e4fbc00c6f010e2d3
SosoG-null <144150345+MGaspera@users.noreply.github.com>
2023-09-17 13:08:09 +0100

    Enemies
    Starting to add in enemies

--- Classes.py
+++ Classes.py
@@ -1,49 +1,111 @@
 import pygame
 import os
+import random
 
+# Define the Enemy class
+class Enemy:
+    def __init__(self, width, height, image_folder_walk, image_folder_death, speed, walkAssetName, deathAssetName, start_x, start_y):
+        self.width = width
+        self.height = height
+        self.images = [pygame.image.load(os.path.join(image_folder_walk, f'{walkAssetName}-({i}).png')) for i in range(1, 9)]
+        self.death_images = [pygame.image.load(os.path.join(image_folder_death, f'{deathAssetName}-({i}).png')) for i in range(1, 11)]
+        self.left_images = [pygame.transform.flip(image, True, False) for image in self.images]
+        self.x = start_x
+        self.y = start_y
+        self.speed = speed
+        self.direction = 1
+        self.alive = True
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.death_animation_frame = 0
+        self.pursuing = False  # Initialize pursuing flag
+
+    def wandering(self):
+        pass
+
+
+    def pursue_player(self, player_x, player_y):
+            ENEMY_PURSUE_DISTANCE = 100
+            if not self.pursuing:
+                if abs(self.x - player_x) <= ENEMY_PURSUE_DISTANCE:
+                    self.pursuing = True
+            if self.pursuing:
+                if self.x < player_x:
+                    self.direction = 0.8
+                else:
+                    self.direction = -0.8
+                self.x += self.speed * self.direction
+
+                # Move vertically towards the player's y-coordinate
+                if self.y < player_y:
+                    self.direction = 0.8
+                else:
+                    self.direction = -0.8
+                self.y += self.speed * self.direction
+
+
+# Subclass: Bringer of Death
+class BoD(Enemy):
+    def __init__(self):
+        super().__init__(width=57,
+                         height=55,
+                         image_folder_walk="./Assets/Enemies/BoD/Walk",
+                         image_folder_death="./Assets/Enemies/BoD/Death",
+                         deathAssetName="BoDeath",
+                         walkAssetName="BoDWalk",
+                         speed=3)
+
+
+# Define the Player class
 class Player:
     def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.width = 30
-        self.height = 50
-        self.vel = 0.1
+        self.x = 0  # Initial x-coordinate
+        self.y = 0  # Initial y-coordinate
+        self.width = 30  # Width of the player character
+        self.height = 50  # Height of the player character
+        self.vel = 3 # Velocity for player movement
 
+        # Lists of player character images for different animations
         self.idle = [pygame.image.load(os.path.join("./Assets/Necro/Idle/", f'Idle-({i}).png')) for i in range(1, 17)]
         self.death = [pygame.image.load(os.path.join("./Assets/Necro/Death/", f'Death-({i}).png')) for i in range(1, 10)]
         self.move = [pygame.image.load(os.path.join("./Assets/Necro/Move/", f'Move-({i}).png')) for i in range(1, 9)]
-        self.leftIdle = [pygame.transform.flip(image, True, False) for image in self.idle]
+        self.leftIdle = [pygame.transform.flip(image, True, False) for image in self.idle]  # Flipped idle images
 
+    # Function to handle collision with other sprite groups (not implemented yet)
     def collide(self, spriteGroup):
         if pygame.sprite.spritecollide(self, spriteGroup, False):
             pass
 
+# Define the Screen class
 class Screen:
     def __init__(self):
+        # Load the game screen background image
         self.bg = pygame.image.load(os.path.join("./Assets/Bg", f'Ground.jpg'))
 
+# Define the Bullet class
 class Bullet:
+    # Define constants for bullet directions
     LEFT = (-1, 0)
     RIGHT = (1, 0)
     UP = (0, -1)
     DOWN = (0, 1)
 
     def __init__(self, x, y, direction):
-        self.x = x
-        self.y = y
-        self.speed = 0.1
-        self.direction = direction
-        self.rotationAngle = 0  # Initial rotation angle
+        self.x = x  # Initial x-coordinate of the bullet
+        self.y = y  # Initial y-coordinate of the bullet
+        self.speed = 5  # Speed of the bullet
+        self.direction = direction  # Initial direction of the bullet
+        self.rotationAngle = 0  # Initial rotation angle of the bullet
         self.originalImage = pygame.Surface((20, 20), pygame.SRCALPHA)  # Create a transparent surface for the bullet
         pygame.draw.polygon(self.originalImage, (200, 10, 4), [(10, 0), (0, 20), (20, 20)])  # Create a triangular bullet
-        self.image = self.originalImage
-        self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.image = self.originalImage  # Initialize the bullet image
+        self.rect = self.image.get_rect(center=(self.x, self.y))  # Get the bullet's rect for positioning
+        
 
     def update(self):
         # Move the bullet in the specified direction
         self.x += self.speed * self.direction[0]
         self.y += self.speed * self.direction[1]
-        self.rect.center = (self.x, self.y)
+        self.rect.center = (self.x, self.y)  # Update the bullet's position based on (x, y)
 
         # Rotate the bullet (you can adjust the rotation speed by changing the angle increment)
         self.rotationAngle += 0.1  # Adjust the rotation speed here
@@ -52,4 +114,5 @@ class Bullet:
         self.image = pygame.transform.rotate(self.originalImage, self.rotationAngle)
 
     def draw(self, win):
+        # Draw the bullet on the game window
         win.blit(self.image, self.rect)
--- Main.py
+++ Main.py
@@ -1,24 +1,41 @@
 import pygame
 import time
-from Classes import Screen, Player, Bullet
+import random
+from Classes import Screen, Player, Bullet, Enemy
 
+# Initialize pygame
 pygame.init()
 
+# Create the game window
 win = pygame.display.set_mode((500, 500), 0, 0)
 pygame.display.set_caption("JourneyOfThePrairieKing")
 
-# Class instances
-playerSprite = Player()
-gameScreen = Screen()
+
+ENEMY_SPAWN_MIN_INTERVAL = 125  # 1/8th of a second (in milliseconds)
+ENEMY_SPAWN_MAX_INTERVAL = 500  # 1/2 of a second (in milliseconds)
+ENEMY_START_X = random.randint(0, 500 - 55)  # Initial x-coordinate for off-screen spawning
+ENEMY_START_Y = random.randint(0, 500 - 55)  # Initial y-coordinate (random within screen height)
+ENEMY_PURSUE_DISTANCE = 100  # Distance from the player to start pursuing
+ENEMY_MOVE_SPEED = 2  # Speed at which enemies move
+lastEnemySpawnTime = pygame.time.get_ticks()
+
+
+
+# Create instances of the game objects
+playerSprite = Player()  # Player character
+gameScreen = Screen()    # Game screen background
+enemies = []
+playerHitbox = pygame.Rect(playerSprite.x + 20, playerSprite.y + 35, playerSprite.width - 10, playerSprite.height - 10)
 
 # Initial sprite and animation variables
 spriteMain = playerSprite.idle[0]  # Set the initial sprite
 animationSpeed = 100  # Speed of animation (milliseconds)
 lastUpdate = pygame.time.get_ticks()  # Get the initial time
+animationTimer = pygame.time.get_ticks()
 walkCount = 0  # Initialize walkCount
-playerHitbox = pygame.Rect(playerSprite.x + 20, playerSprite.y + 35, playerSprite.width - 10, playerSprite.height - 10)
 playerAnimationState = "idle"  # Initialize playerAnimationState
 
+# Function to create wall Rect objects
 def createWalls():
     # Create Rect objects for the walls and store them in variables
     left = pygame.Rect(0, 0, 10, 500)
@@ -28,6 +45,7 @@ def createWalls():
 
     return left, top, bottom, right
 
+# Function to redraw the game window
 def redrawGameWindow():
     global walkCount, bullets
     win.blit(gameScreen.bg, (0, 0))  # Blit the background first
@@ -39,23 +57,47 @@ def redrawGameWindow():
     pygame.draw.rect(win, (84, 80, 69), bottom)
     pygame.draw.rect(win, (84, 80, 69), right)
 
+    # Draw a green rectangle around the player to represent the no-spawn zone
+    # no_spawn_rect = pygame.Rect(playerSprite.x - 100, playerSprite.y - 100, 200, 200)
+    # pygame.draw.rect(win, (0, 255, 0), no_spawn_rect, 2)  # Green outline
+
     # Update and draw bullets
     for bullet in bullets:
         bullet.update()  # Update bullet position
         bullet.draw(win)  # Draw bullet on the screen
 
-    playerHitbox = pygame.Rect(playerSprite.x + 20, playerSprite.y + 35, playerSprite.width - 10, playerSprite.height - 10)
-    pygame.draw.rect(win, (255, 0, 0), playerHitbox, 2)  # Red outline
+    # Draw BoD enemies and handle death animations
+    for enemy in enemies:
+        if enemy.alive:
+            if enemy.direction == 1:
+                win.blit(enemy.images[walkCount % len(enemy.images)], (enemy.x, enemy.y))
+            else:
+                win.blit(enemy.left_images[walkCount % len(enemy.left_images)], (enemy.x, enemy.y))
+        else:
+            if walkCount // 10 < len(enemy.death_images):
+                win.blit(enemy.death_images[walkCount // 10], (enemy.x, enemy.y))
+            else:
+                enemies.remove(enemy)
+
+
+
     win.blit(spriteMain, (playerSprite.x, playerSprite.y))  # Blit the player sprite
     pygame.display.update()  # Update the display
 
 
-def updateAnimation(currentTime):
-    global walkCount, lastUpdate, playerAnimationState, spriteMain
+def updateAnimation():
+    global playerAnimationState, spriteMain, animationTimer, walkCount, keys
+
+    # Calculate the time passed since the last animation frame change
+    currentTime = pygame.time.get_ticks()
+    timePassed = currentTime - animationTimer
+
+    # Debug output to check playerAnimationState
+    #print(f"Player Animation State: {playerAnimationState}")
 
     # Check if it's time to update the animation frame
-    if currentTime - lastUpdate >= animationSpeed:
-        lastUpdate = currentTime
+    if timePassed >= animationSpeed:
+        animationTimer = currentTime  # Reset the animation timer
 
         # Default sprite when not moving
         spriteMain = playerSprite.idle[walkCount % len(playerSprite.idle)]
@@ -76,28 +118,65 @@ def updateAnimation(currentTime):
 
 
 
+
+# Initialize movement flags
 right = False
-left = False 
+left = False
 
 # Set boundaries
 screenWidth = 500
 screenHeight = 500
 
 # Define the borderWidth (how close to the edge the player can get)
-borderWidth = 5  
+borderWidth = 5
 
 # Create a list to store bullets
 bullets = []
 
 lastShotTime = 0
 run = True
-run = True
+
+# Constants for frame rate and time step
+FPS = 60
+TIME_STEP = 1000 / FPS  # Time step in milliseconds
+
+# Main game loop
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    for bullet in bullets:
+        # Check collision between bullet and enemies
+        for enemy in enemies:
+            if enemy.alive and playerHitbox.colliderect(enemy.rect):
+                bullet.kill()  # Remove the bullet
+                enemy.die()    # Mark the enemy as dead
+
+
+    #print(type(playerHitbox))
+    #print(type(Enemy))
+
+
+    # Player-enemy collision detection
+    for enemy in enemies:
+        if enemy.alive:
+            enemy.pursue_player(playerSprite.x, playerSprite.y)
+            if enemy.direction == 1:
+                win.blit(enemy.images[walkCount % len(enemy.images)], (enemy.x, enemy.y))
+            else:
+                win.blit(enemy.left_images[walkCount % len(enemy.left_images)], (enemy.x, enemy.y))
+        else:
+            if enemy.death_animation_frame < len(enemy.death_images):
+                win.blit(enemy.death_images[enemy.death_animation_frame], (enemy.x, enemy.y))
+                enemy.death_animation_frame += 1
+            else:
+                enemies.remove(enemy)
+
+    # Calculate time passed since the last frame
     currentTime = pygame.time.get_ticks()
+    deltaTime = currentTime - lastUpdate
+    lastUpdate = currentTime
 
     # Calculate the center of the player's hitbox
     centerX = playerSprite.x + playerSprite.width // 2
@@ -109,17 +188,18 @@ while run:
     nextX = playerSprite.x
     nextY = playerSprite.y
 
+    # Handle player movement
     if keys[pygame.K_a]:
-        nextX -= playerSprite.vel
+        nextX -= playerSprite.vel * deltaTime / TIME_STEP
         playerAnimationState = "walk"
     elif keys[pygame.K_d]:
-        nextX += playerSprite.vel
+        nextX += playerSprite.vel * deltaTime / TIME_STEP
         playerAnimationState = "walk"
 
     if keys[pygame.K_w]:
-        nextY -= playerSprite.vel
+        nextY -= playerSprite.vel * deltaTime / TIME_STEP
     elif keys[pygame.K_s]:
-        nextY += playerSprite.vel
+        nextY += playerSprite.vel * deltaTime / TIME_STEP
 
     nextPlayerHitbox = pygame.Rect(nextX + 20, nextY + 35, playerSprite.width - 10, playerSprite.height - 10)
 
@@ -141,6 +221,7 @@ while run:
 
     bulletDirection = (0, 0)
 
+    # Handle shooting in different directions
     if keys[pygame.K_LEFT]:
         bulletDirection = (-1, 0)
     elif keys[pygame.K_RIGHT]:
@@ -161,6 +242,7 @@ while run:
 
     timeSinceLastShot = currentTime - lastShotTime
 
+    # Create bullets based on the player's input and shooting cooldown
     if timeSinceLastShot >= 250 and bulletDirection != (0, 0):
         startX = centerX + 10
         startY = centerY + 20
@@ -168,15 +250,57 @@ while run:
         bullets.append(newBullet)
         lastShotTime = currentTime
 
+    # Remove bullets that have gone off-screen
     bullets = [bullet for bullet in bullets if bullet.y > 0]
 
+    # Spawn BoD enemies
+    if currentTime - lastEnemySpawnTime >= random.randint(ENEMY_SPAWN_MIN_INTERVAL, ENEMY_SPAWN_MAX_INTERVAL):
+        ENEMY_START_X = random.randint(0, 500 - 55)
+        ENEMY_START_Y = random.randint(0, 500 - 55)
+        newEnemy = Enemy(57, 55, "./Assets/Enemies/BoD/Walk", "./Assets/Enemies/BoD/Death", ENEMY_MOVE_SPEED, "BoDWalk", "BoDeath", ENEMY_START_X, ENEMY_START_Y)
+        enemies.append(newEnemy)
+        lastEnemySpawnTime = currentTime  # Update the last spawn time
+
+
+
+    # Update BoD enemy behavior
+    for enemy in enemies:
+        if enemy.alive:
+            if enemy.direction == 1:
+                win.blit(enemy.images[walkCount % len(enemy.images)], (enemy.x, enemy.y))
+            else:
+                win.blit(enemy.left_images[walkCount % len(enemy.left_images)], (enemy.x, enemy.y))
+        else:
+            if enemy.death_animation_frame < len(enemy.death_images):
+                win.blit(enemy.death_images[enemy.death_animation_frame], (enemy.x, enemy.y))
+                enemy.death_animation_frame += 1
+            else:
+                enemies.remove(enemy)
+
+
+    # Clear the screen and update the animation
     win.fill((0, 0, 0))
     win.blit(gameScreen.bg, (0, 0))
-
-    # Update the animation
-    updateAnimation(currentTime)
-
+    updateAnimation()
     win.blit(spriteMain, (playerSprite.x, playerSprite.y))
+
+    # Draw BoD enemies and handle death animations
+    for enemy in enemies:
+        if enemy.alive:
+            if enemy.direction == 1:
+                win.blit(enemy.images[walkCount % len(enemy.images)], (enemy.x, enemy.y))
+            else:
+                win.blit(enemy.left_images[walkCount % len(enemy.left_images)], (enemy.x, enemy.y))
+        else:
+            if walkCount // 10 < len(enemy.death_images):
+                win.blit(enemy.death_images[walkCount // 10], (enemy.x, enemy.y))
+            else:
+                enemies.remove(enemy)
+
     redrawGameWindow()
 
+    # Cap the frame rate
+    pygame.time.Clock().tick(FPS)
+
+# Quit pygame when the game loop exits
 pygame.quit()
